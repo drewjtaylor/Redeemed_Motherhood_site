0510fb6a3d1cb7bcc46a7f2e522be3e87188d6ad
Andrew Taylor <drew.j.taylor1@gmail.com>
2022-04-06 16:24:30 -0400

    django now talking to pg in container

--- rm_django_app/rm_django_app/settings.py
+++ rm_django_app/rm_django_app/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'rm_django_app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'test_db',
+        'USER': 'postgres',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': 5432
     }
 }
 
