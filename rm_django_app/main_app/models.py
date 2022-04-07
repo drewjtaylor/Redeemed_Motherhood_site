9285cfe5b7b8c63ca23d49c0a3f6acf5c0730c85
Andrew Taylor <drew.j.taylor1@gmail.com>
2022-04-07 02:51:21 -0400

    simpler table elements created

--- rm_django_app/main_app/models.py
+++ rm_django_app/main_app/models.py
@@ -2,6 +2,17 @@ from django.db import models
 
 # Create your models here.
 
+
+class Providers(models.Model):
+    name = models.CharField("Business name", max_length=50)
+    contact = models.CharField("Contact person", max_length=30)
+    email = models.CharField(max_length=50)
+    phone = models.CharField("Phone number", max_length=10, )
+
 class Videos(models.Model):
     title = models.CharField(max_length=35)
     link = models.CharField(max_length=100)
+
+class Invoices(models.Model):
+    amount = models.DecimalField(max_digits=5, decimal_places=2)
+#    owing_client = models.ForeignKey(Client, on_delete=models.CASCADE)
