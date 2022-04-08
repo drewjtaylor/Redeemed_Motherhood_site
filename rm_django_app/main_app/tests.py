18c53c4bf5edb5e39df55611f48fdc2f57c06670
Andrew Taylor <drew.j.taylor1@gmail.com>
2022-04-08 18:28:15 -0400

    added test for json serialization

--- rm_django_app/main_app/tests.py
+++ rm_django_app/main_app/tests.py
@@ -1,9 +1,12 @@
 from django.test import TestCase
 from .models import Client
 import pytest
+from django.core import serializers
+import json
 
 # Create your tests here.
 
+#Fixure client to be used for testing
 @pytest.fixture
 def client():
     fakeclient = Client(
@@ -20,7 +23,16 @@ def client():
     )
     return fakeclient
 
+# Test to see if clients can be added to database
 @pytest.mark.django_db
 def test_add_client(client):
     client.save()
     assert Client.objects.filter(username="fake_user")
+
+@pytest.mark.django_db
+def test_serialize_client(client):
+    client.save()
+    data = serializers.serialize('json', Client.objects.filter(username="fake_user"))
+    pydata = json.loads(data)
+    assert 'fake_user' == (pydata[0]['fields']['username'])
+
