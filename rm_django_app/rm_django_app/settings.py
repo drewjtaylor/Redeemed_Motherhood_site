ab20f4cde14538aca2a000d1c0da80b7804da29c
Andrew Taylor <drew.j.taylor1@gmail.com>
2022-04-08 16:52:57 -0400

    minor cleanliness update

--- rm_django_app/rm_django_app/settings.py
+++ rm_django_app/rm_django_app/settings.py
@@ -38,8 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-
-    # Add install apps for project
+    # Add/install apps for project
     'main_app',
 ]
 
