b410c69bbcfc10424c24f80d54056ddd84e31a28
Andrew Taylor <drew.j.taylor1@gmail.com>
2022-04-08 15:46:45 -0400

    Started writing tests

--- rm_django_app/main_app/tests.py
+++ rm_django_app/main_app/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+from .models import Client
+import pytest
 
 # Create your tests here.
+
+# @pytest.fixture
+# def client():
+#     return Client.objects.create(is_active=True)
+
+# @pytest.mark.django_db
+# def test_deactivate(client):
+#     client.deactivate()
+#     assert not client.is_active
+
+# @pytest.mark.django_db
+# def test_deactivate_reactivate(client):
+#     client.deactivate()
+#     client.activate()
+#     assert client.is_active
