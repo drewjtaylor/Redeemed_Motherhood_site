198447230a4e32798d060874bc78c9717494a665
Andrew Taylor <drew.j.taylor1@gmail.com>
2022-04-08 16:53:09 -0400

    Test to create user implemented

--- rm_django_app/main_app/tests.py
+++ rm_django_app/main_app/tests.py
@@ -4,17 +4,23 @@ import pytest
 
 # Create your tests here.
 
-# @pytest.fixture
-# def client():
-#     return Client.objects.create(is_active=True)
+@pytest.fixture
+def client():
+    fakeclient = Client(
+        password="das8d97a@##", 
+        username="fake_user", 
+        first_name="Andrea", 
+        last_name="Taylor", 
+        email="not_real@mailinator.com",
+        is_staff=True,
+        is_active=True, 
+        phone="7275551234",
+        partner_name="Andrew", 
+        partner_email="husbando@mailinator.com",
+    )
+    return fakeclient
 
-# @pytest.mark.django_db
-# def test_deactivate(client):
-#     client.deactivate()
-#     assert not client.is_active
-
-# @pytest.mark.django_db
-# def test_deactivate_reactivate(client):
-#     client.deactivate()
-#     client.activate()
-#     assert client.is_active
+@pytest.mark.django_db
+def test_add_client(client):
+    client.save()
+    assert Client.objects.filter(username="fake_user")
