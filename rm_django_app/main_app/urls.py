ba696cbfef4a71a475ef9bcda59a64ce0b9048ce
Andrew Taylor <drew.j.taylor1@gmail.com>
2022-04-19 17:32:27 -0400

    create sign up page at home/signup

--- rm_django_app/main_app/urls.py
+++ rm_django_app/main_app/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.conf.urls import url
 
 from . import views
 
@@ -7,4 +8,5 @@ urlpatterns = [
     path('index/', views.index, name='index'),
     path('clients/', views.clients, name='clients'),
     path('invoices/', views.invoices, name='invoices'),
+    url('signup/', views.signup, name='signup'),
 ]
