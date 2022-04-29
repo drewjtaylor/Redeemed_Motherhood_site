e1bb7124a7b8ae3c69869e024d62a4d5fc32145d
Andrew Taylor <drew.j.taylor1@gmail.com>
2022-04-29 12:37:14 -0400

    Require login and group permission for video

--- rm_django_app/videos/views.py
+++ rm_django_app/videos/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from main_app.models import Client, Video
+from django.contrib.auth.decorators import login_required, user_passes_test
 
 # Create your views here.
 
@@ -9,6 +10,8 @@ def index(request):
     context = {'video_list': video_list}
     return render(request, 'videos/index.html', context)
 
+@login_required
+@user_passes_test(lambda u: u.groups.filter(name='BoL_series').count() == 0, login_url='/myapp/denied/')
 def viewvid(request, video_id):
     video = Video.objects.get(pk=video_id)
     context = {'video_id': video_id, 'video': video}
